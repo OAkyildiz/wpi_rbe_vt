6cad681bd54b056a983269dafb39d34e32eb067b
Ozan Akyildiz <oakyildiz@wpi.edu>
2018-08-27 17:34:19 -0400

    markers and stuff

---
+++ res/other/labs.js
@@ -0,0 +1,15 @@
+var labs={
+"type": "FeatureCollection",
+"name": "Labs",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "AK 120", "description": "Undergraduate Classes" }, "geometry": { "type": "Point", "coordinates": [ -71.8067814, 42.2751644 ] } },
+{ "type": "Feature", "properties": { "Name": "CIBR Lab", "description": "Human Robot Interaction and Path Planning" }, "geometry": { "type": "Point", "coordinates": [ -71.7997796, 42.2762088 ] } },
+{ "type": "Feature", "properties": { "Name": "AIM Lab", "description": "Medical " }, "geometry": { "type": "Point", "coordinates": [ -71.8000035, 42.2760505 ] } },
+{ "type": "Feature", "properties": { "Name": "NEST Lab", "description": "Swarm" }, "geometry": { "type": "Point", "coordinates": [ -71.7998258, 42.2761275 ] } },
+{ "type": "Feature", "properties": { "Name": "Onal Shop", "description": null }, "geometry": { "type": "Point", "coordinates": [ -71.7997608, 42.2759325 ] } },
+{ "type": "Feature", "properties": { "Name": "Popovic Lab", "description": null }, "geometry": { "type": "Point", "coordinates": [ -71.799687, 42.2759374 ] } },
+{ "type": "Feature", "properties": { "Name": "Robotic Navigation and Communications Lab", "description": null }, "geometry": { "type": "Point", "coordinates": [ -71.7998237, 42.2760843 ] } },
+{ "type": "Feature", "properties": { "Name": "COMET Lab", "description": null }, "geometry": { "type": "Point", "coordinates": [ -71.7997661, 42.2760476 ] } }
+]
+};
--- src/main.js
+++ src/main.js
@@ -16,6 +16,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var wpi = {lat:42.2751, long:-71.8053, def_zoom:17};
 var wpi_style = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#785b5b"},{"visibility":"simplified"}]},{"featureType":"landscape.man_made","elementType":"labels.text.fill","stylers":[{"visibility":"simplified"},{"color":"#400707"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#4a1616"}]},{"featureType":"poi.sports_complex","elementType":"labels.text.fill","stylers":[{"color":"#350e0e"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"hue":"#ff0000"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+//TODO: Make symbosl for markers
+var markers={
+  size: 10
+  , simple: {ico: 'res/ico/circle_ico.png', color: 'red'}
+  , lab: {ico: 'res/ico/tools.svg', color: '#902A20'}
+  , office: {ico: 'res/ico/desk.png', color: '#32607A'}
+  , project: {ico:'res/ico/gear.png', color:'#323232'}
+  , outlineColor: '#606060',
+  };
+
+
 var map;
 var build = true;
 function initMap(){
@@ -35,39 +46,152 @@ function initMap(){
     fullscreenControl: false,
     styles: wpi_style
   });
-
-
-
-
+    google.maps.Polygon.prototype.getPolygonBounds=function(){
+        var bounds = new google.maps.LatLngBounds()
+        this.getPath().forEach(function(element,index){bounds.extend(element)})
+        return bounds
+    }
 var buildings;
 var labs;
 var offices;
 var path;
 
+var layers = new Map();
 
- buildings = new google.maps.KmlLayer('https://sites.google.com/site/wpirbemaprepo/home/files/buildings.kml', {
-  suppressInfoWindows: true,
-  preserveViewport: false,
-  map: map
-// if (build){
+
+layers['buildings'] = new google.maps.Data();
+layers['markers'] = new google.maps.Data();
+layers['path'] = new google.maps.Data();
+
+layers['buildings'].addListener('addfeature',function(e){
+  buildingCallback(e.feature) ;
 });
-labs = new google.maps.KmlLayer('https://sites.google.com/site/wpirbemaprepo/home/files/labs.kml', {
-  suppressInfoWindows: true,
-  preserveViewport: false,
-  map: map
-// if (build){
+
+layers['buildings'].addListener('click', function(event) {
+  var feature = event.feature;
+  if(feature.getGeometry().getType()=='Polygon'){
+      feature.setProperty('focused', true); //set all others o false
+      var bound = feature.getPolygonBounds();
+      map.fitBounds(bounds);
+      console.log(feature.getProperty('name'));
+    }
+    else{
+    console.log('?');
+    }
+  });
+
+
+layers['markers'].addListener('addfeature',function(e){
+  markerCallback(e.feature) ;
 });
- offices = new google.maps.KmlLayer('https://sites.google.com/site/wpirbemaprepo/home/files/offices.kml', {
-  suppressInfoWindows: true,
-  preserveViewport: false,
-  map: map
-// if (build){
+//layers['labs'].setMap(map);
+
+layers['buildings'].setMap(map);
+layers['markers'].setMap(map);
+layers['path'].setMap(map);
+
+
+layers['buildings'].loadGeoJson('res/json/buildings.geojson', null);/*buildingsCallback(layers['buildings']));*/
+layers['path'].loadGeoJson('res/json/path1.geojson', null);
+layers['markers'].loadGeoJson('res/json/labs.geojson', null);
+layers['markers'].loadGeoJson('res/json/offices.geojson', null);/*,markerCallback(layers['offices']));*/
+
+//layers['offices'].setMap(map)
+layers['markers'].setStyle( {
+
+  icon: {
+    path: google.maps.SymbolPath.CIRCLE
+    , scale: 5
+    , strokeColor: markers.outlineColor
+    , strokeWeight: 1.5
+// fillColor:'#202020'
+    , fillOpacity: 1
+  }
 });
- path = new google.maps.KmlLayer('https://sites.google.com/site/wpirbemaprepo/home/files/path1.kml', {
-  suppressInfoWindows: true,
-  preserveViewport: false,
-  map: map
-// if (build){
+
+layers['buildings'].setStyle( {
+  fillColor: '#903737',
+  strokeColor: '#503030',
+  strokeWeight: 2,
 });
+
+layers['path'].setStyle( {
+    strokeColor: '#202020'
+    ,icon: {
+        path: google.maps.SymbolPath.CIRCLE
+        , scale: 5
+        , strokeColor: '#202020'
+        , StrokeColor: markers.outlineColor
+        , strokeWeight: 0.75
+        , fillColor:'#202020'
+        , fillOpacity: 1
+
+    // size: new google.maps.Size(markers.size, markers.size),
+    // origin: new google.maps.Point(0, 0),
+    // anchor: new google.maps.Point(markers.size/2, markers.size/2),
+    // scaledSize: new google.maps.Size(markers.size, markers.size)
+  }
+});
+//
+
+// console.log(map.data);
+// map.data.forEach(function(feature) {
+//     console.log('>> ');
+// });
+console.log("huh");
 
 }
+
+
+
+/////////////////
+  /* Helpers */
+/////////////////
+
+
+
+function check_cb(){ console.log('done')}
+
+
+function markerCallback(marker){
+    var c=0;
+    console.log('hit');
+    console.log(marker.getProperty('name') , ', ',marker.getProperty('type'),', ', marker.getId(),': ', marker.getGeometry().getType());
+    // marker.setStyle({
+    //   icon:{
+    //     fillColor: markers.lab.color
+    //;  }
+
+}
+
+function labsCallback(json){
+  labs=json;
+  console.log(labs);
+}
+
+function  buildingCallback(feature){
+
+  //  map.Data.setMap(map);
+// Feature related stuff here
+    console.log(feature.getProperty('name'));
+  }
+  // buildings=json;
+  // console.log('wth');
+  // console.log(buildings);
+  //
+  // $.each(buildings, function(idx, loc) {
+  // //draw
+  // console.log('drawing buildings');
+  // loc.dwg = new google.maps.Polygon({
+  //         paths: loc.coordinates,
+  //         strokeColor: '#FF0000',
+  //         strokeOpacity: 0.8,
+  //         strokeWeight: 3,
+  //         fillColor: '#FF0000',
+  //         fillOpacity: 0.35
+  //       });
+  // loc.dwg.setMap(map);
+  // // add listener
+  //
+  // // add counter
+  //   loc.count =0;
